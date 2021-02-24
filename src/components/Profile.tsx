22ecd08eb422cfc70cbef16a44844a8a68871643
Gabriel Pires <gabriel.santos.pes@gmail.com>
2021-02-24 10:33:35 -0300

    Css da home em modulo Criação do component profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/GabriPires.png" alt="Gabriel Pires"/>
+      <div>
+        <strong>Gabriel Pires</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
