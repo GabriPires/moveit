2d80fc8e60ad4980e6d52e18fffcc575f5b7469b
Gabriel Pires <gabriel.santos.pes@gmail.com>
2021-02-25 11:36:36 -0300

    Tipagem do contexto Chamando função do contexto

--- src/contexts/ChallengeContext.tsx
+++ src/contexts/ChallengeContext.tsx
@@ -1,20 +1,42 @@
 import { createContext, ReactNode, useState } from 'react';
 
-export const ChallengesContext = createContext({});
+interface ChallengeContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
 
 interface ChallengesProviderProps {
   children: ReactNode;
 }
 
+export const ChallengesContext = createContext({} as ChallengeContextData);
+
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengeCompleted] = useState(0);
 
   function levelUp() {
     setLevel(level + 1);
   }
 
+  function startNewChallenge() {
+    console.log('New Challenge')
+  }
+
   return (
-    <ChallengesContext.Provider value={{ level, levelUp }}>
+    <ChallengesContext.Provider
+      value={{ 
+        level, 
+        currentExperience, 
+        challengesCompleted, 
+        levelUp,
+        startNewChallenge
+      }}
+    >
       {children}
     </ChallengesContext.Provider>
   );
